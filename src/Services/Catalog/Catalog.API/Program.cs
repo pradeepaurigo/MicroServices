63a158c0c856693085bde977a28369b454bf35fb
PradeepKumar Verma <pradeepkumar.verma@Aurigo.com>
2023-07-13 16:27:01 +0530

    Catalog Service

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,33 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( c =>
+                                {
+                                    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+                                });
+
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json","Catalog.API V1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
